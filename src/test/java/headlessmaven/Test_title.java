e72ab282d22f3cb96e2ac136db485a124e8c64e5
vilas <80336217+vilas-92@users.noreply.github.com>
2023-11-07 10:08:48 +0530

    Update Test_title.java
    creating instance

--- src/test/java/headlessmaven/Test_title.java
+++ src/test/java/headlessmaven/Test_title.java
@@ -24,7 +24,7 @@ public class Test_title {
 	@BeforeTest
 	public void BeforeOpen() {
 
-
+		// creating instance of ChromeOption
 		ChromeOptions option = new ChromeOptions();
 		option.addArguments("--headless=new");
 
